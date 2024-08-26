b2c3d41c991588cfaef720e4adc18b3136f1aba4
Baishali-7 <baishalima7@gmail.com>
2024-08-26 11:55:48 +0545

    added js and cs

---
+++ public/js/script.js
@@ -0,0 +1,8 @@
+const socket = io();
+
+if(navigator.geolocation){
+    navigator.geolocation.watchPosition((position)=>{
+       const{latitude,longitude} = position.coords
+       socket.emit("")
+    })
+}
