c9b468b374194d9614189dfac74500e80d5d38f3
Baishali-7 <baishalima7@gmail.com>
2024-08-26 15:54:05 +0545

    added location

--- app.js
+++ app.js
@@ -1,25 +1,48 @@
-const express = require('express')
-const app = express()
-const http = require('http')
-const path = require('path')
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const socketIO = require('socket.io');
 
-const socket = require('socket.io')
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
-const server = http.createServer(app)
-
-const io = socket(server)
-
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, 'public')));
+
 console.log('Serving static files from:', path.join(__dirname, 'public'));
 
-io.on("connection",function(socket){
-    console.log("connected")
-})
 
-app.get('/',function(req,res){
-    res.render('index')
-})
+io.on("connection", (socket) => {
+    console.log(`User connected: ${socket.id}`);
 
-server.listen(3000)
+   
+    socket.on("send-location", (data) => {
+        console.log(`Received location from ${socket.id}:`, data);
 
+       
+        const { latitude, longitude } = data;
+        if (typeof latitude === 'number' && typeof longitude === 'number') {
+            
+            io.emit("receive-location", { id: socket.id, latitude, longitude });
+        } else {
+            console.warn(`Invalid location data received from ${socket.id}:`, data);
+        }
+    });
+
+   
+    socket.on('disconnect', () => {
+        console.log(`User disconnected: ${socket.id}`);
+        io.emit('user-disconnected', socket.id);
+    });
+});
+
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+
+server.listen(3000, () => {
+    console.log('Server is listening on http://localhost:3000');
+});
--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,60 @@
-const socket = io();
+const socket = io(); 
 
-if(navigator.geolocation){
-    navigator.geolocation.watchPosition((position)=>{
-       const{latitude,longitude} = position.coords
-       socket.emit("")
-    })
+
+if (navigator.geolocation) {
+    
+    navigator.geolocation.watchPosition(
+        (position) => {
+            const { latitude, longitude } = position.coords;
+            console.log('Sending location:', { latitude, longitude });
+            socket.emit("send-location", { latitude, longitude });
+        },
+        (error) => {
+            console.error("Geolocation error:", error);
+        },
+        {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0
+        }
+    );
+} else {
+    console.error("Geolocation is not supported by this browser.");
 }
+
+const map = L.map("map").setView([0, 0], 16);
+
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "Map "
+}).addTo(map);
+
+
+const markers = {};
+
+
+socket.on("receive-location", (data) => {
+    const { id, latitude, longitude } = data;
+    console.log('Received location data:', data);
+
+    
+    map.setView([latitude, longitude], 16);
+
+    
+    if (markers[id]) {
+        console.log('Updating marker position for ID:', id);
+        markers[id].setLatLng([latitude, longitude]); 
+    } else {
+        console.log('Adding new marker for ID:', id);
+        markers[id] = L.marker([latitude, longitude]).addTo(map);
+    }
+});
+
+
+socket.on("user-disconnected", (id) => {
+    console.log('User disconnected:', id);
+    if (markers[id]) {
+        map.removeLayer(markers[id]);
+        delete markers[id];
+    }
+});
